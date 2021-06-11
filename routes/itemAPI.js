41db999f4d29f4af3a4c2bdcd16afd45baeabdfa
Nicky Hendrik Sen <nicky.sen@binus.edu>
2021-06-11 15:07:27 +0700

    Enable all cors

--- routes/itemAPI.js
+++ routes/itemAPI.js
@@ -1,8 +1,9 @@
 var express= require("express")
 var db = require('../database/connection')
+var cors = require('cors')
 var router = express.Router()
 
-router.get("/getItem", async function(req, res){
+router.get("/getItem", cors(), async function(req, res){
     try{
         let results = await db.getItem()
         res.json(results)
