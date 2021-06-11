c50df188c62cdeec1ee57862c26f212e67390c15
Nicky Hendrik Sen <nicky.sen@binus.edu>
2021-06-11 14:44:47 +0700

    Added get Item API

--- routes/itemAPI.js
+++ routes/itemAPI.js
@@ -1,8 +1,15 @@
 var express= require("express")
+var db = require('../database/connection')
 var router = express.Router()
 
-router.get("/", function(req, res){
-    res.send("Hello world!")
+router.get("/getItem", async function(req, res){
+    try{
+        let results = await db.getItem()
+        res.json(results)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 module.exports = router;
